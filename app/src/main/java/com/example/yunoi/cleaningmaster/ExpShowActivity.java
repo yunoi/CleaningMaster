3277f504369b13124f89212c0a34fec223311c22
chaehyeon01 <cougs633@naver.com>
2019-12-13 11:32:32 +0900

    남채현-보상화면 디자인(아이콘 추가(메달,빵빠레))

--- app/src/main/java/com/example/yunoi/cleaningmaster/ExpShowActivity.java
+++ app/src/main/java/com/example/yunoi/cleaningmaster/ExpShowActivity.java
@@ -1,5 +1,6 @@
 package com.example.yunoi.cleaningmaster;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,5 +12,6 @@ public class ExpShowActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expshow_layout);
+        getSupportActionBar().hide();
     }
 }
