097dc66fb132ea9e139544b9c0184467b36343e7
JAE HOON PARK <cwpark2193@gmail.com>
2019-12-14 16:12:35 +0900

    박재훈-프로필 생성시 도움말 안보이는 오류 수정, 바텀메뉴 달력 항목 첫번째 생성

---
+++ app/src/main/java/com/example/yunoi/cleaningmaster/CalendarAdapter.java
@@ -0,0 +1,6 @@
+package com.example.yunoi.cleaningmaster;
+
+import android.widget.BaseAdapter;
+
+public class CalendarAdapter extends BaseAdapter {
+}
---
+++ app/src/main/java/com/example/yunoi/cleaningmaster/CalendarFragment.java
@@ -0,0 +1,6 @@
+package com.example.yunoi.cleaningmaster;
+
+import android.support.v4.app.Fragment;
+
+public class CalendarFragment extends Fragment {
+}
