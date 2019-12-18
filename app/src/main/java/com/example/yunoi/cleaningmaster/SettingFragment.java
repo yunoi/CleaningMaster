895c2900ae0a4385a9b349a74853650a5b12508f
chaehyeon01 <cougs633@naver.com>
2019-12-18 14:40:22 +0900

    남채현-옵션메뉴 셋팅 레이아웃 보완

--- app/src/main/java/com/example/yunoi/cleaningmaster/SettingFragment.java
+++ app/src/main/java/com/example/yunoi/cleaningmaster/SettingFragment.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -23,10 +27,49 @@ public class SettingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.setting_fragment, container, false);
-        swNotify = view.findViewById(R.id.swNotify);
+//        swNotify = view.findViewById(R.id.swNotify);
         swTutoCheck = view.findViewById(R.id.swTutoCheck);
 
-        //
+        //액션바 설정
+        ActionBar actionBar = ((MainActivity) getActivity()).getSupportActionBar();
+        // Custom Actionbar를 사용하기 위해 CustomEnabled을 true 시키고 필요 없는 것은 false 시킨다
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
+        actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
+        actionBar.setDisplayShowHomeEnabled(false);//홈 아이콘을 숨김처리합니다.
+
+        View actionbarlayout = inflater.inflate(R.layout.setting_actionbar_layout, null);
+        actionBar.setCustomView(actionbarlayout);
+        //액션바 양쪽 공백 없애기
+        Toolbar parent = (Toolbar) actionbarlayout.getParent();
+        parent.setContentInsetsAbsolute(0, 0);
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        actionBar.setCustomView(actionbarlayout, params);
+
+        //end of 액션바 공백 없애기
+
+        //액션바 버튼
+        ImageButton imageButton = actionbarlayout.findViewById(R.id.actionbar_settingBtnBack);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fragment = new MainFragment(); //돌아가는 fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.coordinatorLayout, fragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        //////////////////////////////////////////////////여기까지 by채현
+
+
+
+
+
         final SharedPreferences passTutorial = getActivity().getSharedPreferences("change",MODE_PRIVATE);
         int tutorialState = passTutorial.getInt("First",0);
         if(tutorialState==1){
